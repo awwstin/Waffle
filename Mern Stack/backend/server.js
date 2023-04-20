f76f4b6318cb43d8524ff51bd47bbe269b83ddd1
awwstin <li.jiashun1703@gmail.com>
2023-04-20 18:54:19 -0500

    l

--- Mern Stack/backend/server.js
+++ Mern Stack/backend/server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 
 app.use(
     cors({
-      origin: ["http://localhost:3000", "https://lwaffle.onrender.com"],
+      origin: ["http://localhost:3000", "https://lwaffle-api.onrender.com"],
     })
   );
 
